de86e213b59ac1afb6fe55eba9bc7c510550dcf3
Bence A. Tóth <tothab@gmail.com>
2022-05-17 09:30:26 +0200

    Added some more aliases as comments for now

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export const aye = true;
 export const certain = true;
 export const definite = true;
 export const positive = true;
+export const right = true;
 export const sure = true;
 export const undoubted = true;
 export const unquestionable = true;
@@ -13,8 +14,62 @@ export const yeah = true;
 export const yep = true;
 export const yes = true;
 export const yup = true;
-export const right = true;
+// absolutely
+// achcha
+// agreed
+// alright
+// alrighty
+// aye
+// ayuh
+// betcha
+// certainly
+// cha
+// chaa
+// definitely
+// defo
+// fine
+// forsooth
+// indeed
+// indubitably
+// kay
+// mHmm
+// mkay
+// ok
+// okay
+// oke
+// okeyDoke
+// okeyDokey
+// right
+// righty
+// rightyO
+// rightyHo
+// roger
+// rogerThat
+// surely
+// totally
+// totes
+// uhHuh
+// undoubtedly
+// unquestionably
+// verily
+// word
+// ya
+// yarp
+// yas
+// ye
+// yepperdoodles
+// yeppers
+// yesh
+// yiss
+// yomp
+// yupperdoodles
+// yuppers
+// yush
+// 👍
+// 👌
 
+export const aHoax = false;
+export const aLie = false;
 export const nay = false;
 export const negative = false;
 export const nix = false;
@@ -22,9 +77,20 @@ export const no = false;
 export const nope = false;
 export const noway = false;
 export const nowise = false;
-export const yeahRight = false;
-export const simplyNotTrue = false;
-export const aLie = false;
-export const aHoax = false;
-export const youMustBeKidding = false;
 export const sham = false;
+export const simplyNotTrue = false;
+export const yeahRight = false;
+export const youMustBeKidding = false;
+// bollocks
+// fakeNews
+// nae
+// nah
+// nahUh
+// naw
+// negatory
+// nizz
+// noNo
+// nonsense
+// rubbish
+// yeahNah
+// 👎
