e4cd037777c1adea0d74e44b355ceeb68abf660f
Bence A. Tóth <tothab@gmail.com>
2022-05-20 12:55:31 +0200

    Added more aliases

--- src/index.js
+++ src/index.js
@@ -1,96 +1,91 @@
+export const absolutely = true;
+export const achcha = true;
 export const affirmative = true;
+export const agreed = true;
+export const alright = true;
+export const alrighty = true;
 export const amen = true;
 export const assuredly = true;
 export const aye = true;
+export const ayuh = true;
+export const betcha = true;
 export const certain = true;
+export const certainly = true;
+export const cha = true;
+export const chaa = true;
 export const definite = true;
+export const definitely = true;
+export const defo = true;
+export const fine = true;
+export const forsooth = true;
+export const indeed = true;
+export const indubitably = true;
+export const kay = true;
+export const mHmm = true;
+export const mkay = true;
+export const ok = true;
+export const okay = true;
+export const oke = true;
+export const okeyDoke = true;
+export const okeyDokey = true;
 export const positive = true;
 export const right = true;
+export const righty = true;
+export const rightyHo = true;
+export const rightyO = true;
+export const roger = true;
+export const rogerThat = true;
 export const sure = true;
+export const surely = true;
+export const totally = true;
+export const totes = true;
+export const uhHuh = true;
 export const undoubted = true;
+export const undoubtedly = true;
 export const unquestionable = true;
+export const unquestionably = true;
+export const verily = true;
+export const word = true;
+export const ya = true;
+export const yarp = true;
+export const yas = true;
+export const ye = true;
 export const yea = true;
 export const yeah = true;
 export const yep = true;
+export const yepperdoodles = true;
+export const yeppers = true;
 export const yes = true;
+export const yesh = true;
+export const yiss = true;
+export const yomp = true;
 export const yup = true;
-// absolutely
-// achcha
-// agreed
-// alright
-// alrighty
-// aye
-// ayuh
-// betcha
-// certainly
-// cha
-// chaa
-// definitely
-// defo
-// fine
-// forsooth
-// indeed
-// indubitably
-// kay
-// mHmm
-// mkay
-// ok
-// okay
-// oke
-// okeyDoke
-// okeyDokey
-// right
-// righty
-// rightyO
-// rightyHo
-// roger
-// rogerThat
-// surely
-// totally
-// totes
-// uhHuh
-// undoubtedly
-// unquestionably
-// verily
-// word
-// ya
-// yarp
-// yas
-// ye
-// yepperdoodles
-// yeppers
-// yesh
-// yiss
-// yomp
-// yupperdoodles
-// yuppers
-// yush
-// 👍
-// 👌
+export const yupperdoodles = true;
+export const yuppers = true;
+export const yush = true;
 
 export const aHoax = false;
 export const aLie = false;
+export const bollocks = false;
+export const fakeNews = false;
+export const nae = false;
+export const nah = false;
+export const nahUh = false;
+export const naw = false;
 export const nay = false;
 export const negative = false;
+export const negatory = false;
 export const nix = false;
+export const nizz = false;
 export const no = false;
+export const noNo = false;
+export const nonsense = false;
 export const nope = false;
 export const noway = false;
 export const nowise = false;
+export const rubbish = false;
 export const sham = false;
 export const simplyNotTrue = false;
+export const yeahNah = false;
 export const yeahRight = false;
 export const youMustBeKidding = false;
-// bollocks
-// fakeNews
-// nae
-// nah
-// nahUh
-// naw
-// negatory
-// nizz
-// noNo
-// nonsense
-// rubbish
-// yeahNah
-// 👎
